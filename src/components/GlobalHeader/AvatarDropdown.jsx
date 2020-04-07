9f02deb5da42018afbba767cc0f18eebef44d162
Jin Peng <jinpengguan@126.com>
2020-04-07 12:28:01 +0800

    clear storage when log

--- src/components/GlobalHeader/AvatarDropdown.jsx
+++ src/components/GlobalHeader/AvatarDropdown.jsx
@@ -14,9 +14,12 @@ class AvatarDropdown extends React.Component {
       const { dispatch } = this.props;
 
       if (dispatch) {
+        sessionStorage.clear()
+        localStorage.clear()
         dispatch({
           type: 'login/logout',
         });
+
       }
 
       return;
